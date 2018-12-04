cbc7df8c256fbe7aaa13bb829f7e30802bf4158e
Yashwanth Dasar <Yashwanth_dasar@mentor.com>
2018-12-04 16:49:32 +0530

    Additional boards support is added

---
+++ scripts/environment/products/imx6solosabresd-exports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NXP_BOARD="imx6solosabresd"
+ROOTFS_FILE="fsl-image-qt5-validation-imx-$MACHINE.tar.bz2"
+ZIMAGE_FILE="zImage"
+DTB_FILE="imx6solosabresd-sdb.dtb"
+
+BUILD_BASE_DIR="/home/nxp-user1/NXP-IMX-BOTH-BUILDS/"
+LAVA_USER="worker4"
+LAVA_WORKER="134.86.254.27"
+
+JENKINS_USER="nxp-user1"
+JENKINS_BUILD_NXP_SCRIPT="$BUILD_BASE_DIR/jenkins-build-nxp-common"
+source "scripts/environment/projects/nxp-linux-exports.sh"
