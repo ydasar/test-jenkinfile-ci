7a766e0765fd5e7b72c93759bc4c3944b80164ec
Yashwanth Dasar <Yashwanth_dasar@mentor.com>
2018-12-10 18:42:09 +0530

    Made changes to suit jenkins requirements

--- scripts/environment/products/imx6slevk-exports.sh
+++ scripts/environment/products/imx6slevk-exports.sh
@@ -5,7 +5,7 @@ ZIMAGE_FILE="zImage"
 DTB_FILE="imx6sl-evk.dtb"
 
 IMAGE_TYPE="fsl-image-qt5-validation-imx"
-BUILD_DIR="build-$TMP_DISTRO-$MACHINE"
+BUILD_DIR="build-qt5-$TMP_DISTRO-$MACHINE"
 EMAIL_NODE_EXPORT_FILE="/tmp/part1_email_exports"
 
 BUILD_BASE_DIR="/home/nxp-user3/NXP-IMX-BOTH-BUILDS/"
